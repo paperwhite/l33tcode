539bd837e01afd189ca33bec76f65fb58dfb7379
Shibani Singh <paperwhite@users.noreply.github.com>
2017-09-28 19:04:16 -0700

    Added problem description

--- add_two_numbers.py
+++ add_two_numbers.py
@@ -3,7 +3,14 @@
 ##    def __init__(self, x):
 #        self.val = x
 #       self.next = None
+"""
+You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
 
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+Output: 7 -> 0 -> 8
+"""
 class Solution(object):
     def carryCheck(self, x, y):
         tmp = x+y
